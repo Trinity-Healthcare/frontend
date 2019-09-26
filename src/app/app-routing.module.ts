6711f9493db299dcf0d815f2b92f3a2cc12f6899
SethCHarris <schristianharris@gmail.com>
2019-09-26 13:47:53 -0500

    Header for admin console in place. Still needs some styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,31 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { HelloworldComponent } from "./helloworld/helloworld.component";
-import { HomeComponent } from "./user/home/home.component";
-import { ActivitiesComponent } from "./user/activities/activities.component";
-import { CalendarComponent } from "./user/calendar/calendar.component";
-import { TeamsComponent } from "./user/teams/teams.component";
-import { PatientsComponent } from "./admin/patients/patients.component";
-import { LoginComponent } from "./user/login/login.component";
-import { RegisterComponent } from "./user/register/register.component";
-import { ProfileComponent } from "./user/profile/profile.component";
-import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
-import { ResetPasswordComponent } from "./reset-password/reset-password.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HelloworldComponent } from './helloworld/helloworld.component';
+import { HomeComponent } from './user/home/home.component';
+import { ActivitiesComponent } from './user/activities/activities.component';
+import { CalendarComponent } from './user/calendar/calendar.component';
+import { TeamsComponent } from './user/teams/teams.component';
+import { PatientsComponent } from './admin/patients/patients.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: "home", component: HomeComponent },
-  { path: "", redirectTo: "/home", pathMatch: "full" },
-  { path: "activities", component: ActivitiesComponent },
-  { path: "calendar", component: CalendarComponent },
-  { path: "teams", component: TeamsComponent },
-  { path: "patients", component: PatientsComponent },
-  { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent },
-  { path: "profile", component: ProfileComponent },
-  { path: "reset", component: ResetPasswordComponent },
-  { path: "forgot", component: ForgotPasswordComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'activities', component: ActivitiesComponent },
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'teams', component: TeamsComponent },
+  { path: 'patients', component: PatientsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'reset', component: ResetPasswordComponent },
+  { path: 'forgot', component: ForgotPasswordComponent },
+  { path: 'dashboard', component: DashboardComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,26 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { HelloworldComponent } from "./helloworld/helloworld.component";
-import { HttpClientModule } from "@angular/common/http";
-import { NavComponent } from "./nav/nav.component";
-import { HomeComponent } from "./user/home/home.component";
-import { ActivitiesComponent } from "./user/activities/activities.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HelloworldComponent } from './helloworld/helloworld.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './user/home/home.component';
+import { ActivitiesComponent } from './user/activities/activities.component';
 
-import { TeamsComponent } from "./user/teams/teams.component";
-import { PatientsComponent } from "./admin/patients/patients.component";
-import { CalendarComponent } from "./user/calendar/calendar.component";
-import { LoginComponent } from "./user/login/login.component";
-import { RegisterComponent } from "./user/register/register.component";
-import { ProfileComponent } from "./user/profile/profile.component";
-import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
-import { ResetPasswordComponent } from "./reset-password/reset-password.component";
-import { FormsModule } from "@angular/forms";
+import { TeamsComponent } from './user/teams/teams.component';
+import { PatientsComponent } from './admin/patients/patients.component';
+import { CalendarComponent } from './user/calendar/calendar.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -33,9 +36,10 @@ import { FormsModule } from "@angular/forms";
     RegisterComponent,
     ProfileComponent,
     ForgotPasswordComponent,
-    ResetPasswordComponent
+    ResetPasswordComponent,
+    DashboardComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
