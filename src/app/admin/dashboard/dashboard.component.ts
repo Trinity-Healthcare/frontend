60937cebcdea4f5c22007d28e6c513dcfef4b36f
SethCHarris <schristianharris@gmail.com>
2019-09-30 21:51:16 -0500

    Reworking dropdown

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 export class DashboardComponent implements OnInit, AfterViewInit {
 
   options: string[] = ['Tasks', 'Users', 'Administrators'];
-  selected: string = this.options[2];
+  selected: string = this.options[0];
 
   columns: HTMLCollectionOf<Element>;
 
