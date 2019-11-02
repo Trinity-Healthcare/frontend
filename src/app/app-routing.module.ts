b88046e01cc910f1165615d04ab1b73ca9a0ae26
sethcharris <schristianharris@gmail.com>
2019-11-02 16:57:53 -0500

    Aesthetic changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,82 +1,84 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { HomeComponent } from "./user/home/home.component";
-import { ActivitiesComponent } from "./user/activities/activities.component";
-import { CalendarComponent } from "./user/calendar/calendar.component";
-import { LoginComponent } from "./user/login/login.component";
-import { RegisterComponent } from "./user/register/register.component";
-import { ProfileComponent } from "./user/profile/profile.component";
-import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
-import { ResetPasswordComponent } from "./reset-password/reset-password.component";
-import { AuthGuardService as AuthGuard } from "./services/auth/auth-guard.service";
-import { AdminDashboardComponent } from "./admin/admin-dashboard/admin-dashboard.component";
-import { AdminPatientsComponent } from "./admin/admin-patients/admin-patients.component";
-import { AdminTasksComponent } from "./admin/admin-tasks/admin-tasks.component";
-import { RoleGuardService } from "./services/auth/role-guard-service.service";
-import { LoginGuardService } from "./services/auth/login-guard.service";
-import { HistoryComponent } from "./history/history.component";
-import { AdminAdminsViewComponent } from "./admin/admin-dashboard/admin-admins-view/admin-admins-view.component";
-import { AdminTaskViewComponent } from "./admin/admin-dashboard/admin-task-view/admin-task-view.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './user/home/home.component';
+import { ActivitiesComponent } from './user/activities/activities.component';
+import { CalendarComponent } from './user/calendar/calendar.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { AuthGuardService as AuthGuard } from './services/auth/auth-guard.service';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { AdminPatientsComponent } from './admin/admin-patients/admin-patients.component';
+import { AdminTasksComponent } from './admin/admin-tasks/admin-tasks.component';
+import { RoleGuardService } from './services/auth/role-guard-service.service';
+import { LoginGuardService } from './services/auth/login-guard.service';
+import { HistoryComponent } from './history/history.component';
+import { AdminAdminsViewComponent } from './admin/admin-dashboard/admin-admins-view/admin-admins-view.component';
+import { AdminTaskViewComponent } from './admin/admin-dashboard/admin-task-view/admin-task-view.component';
 
 const routes: Routes = [
-  { path: "home", component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   {
-    path: "",
-    redirectTo: "/home",
-    pathMatch: "full",
-    canActivate: [AuthGuard]
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+    canActivate: [AuthGuard],
   },
   {
-    path: "activities",
+    path: 'activities',
     component: ActivitiesComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
-  { path: "calendar", component: CalendarComponent, canActivate: [AuthGuard] },
+  { path: 'calendar', component: CalendarComponent, canActivate: [AuthGuard] },
   // { path: "teams", component: TeamsComponent, canActivate: [AuthGuard] },
   {
-    path: "login",
+    path: 'login',
     component: LoginComponent,
-    canActivate: [LoginGuardService]
+    canActivate: [LoginGuardService],
   },
   {
-    path: "register",
+    path: 'register',
     component: RegisterComponent,
-    canActivate: [LoginGuardService]
+    canActivate: [LoginGuardService],
   },
-  { path: "profile", component: ProfileComponent, canActivate: [AuthGuard] },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
   {
-    path: "reset",
-    component: ResetPasswordComponent
-  },
-  {
-    path: "forgot",
-    component: ForgotPasswordComponent
+    path: 'reset',
+    component: ResetPasswordComponent,
   },
   {
-    path: "adminpatients",
+    path: 'forgot',
+    component: ForgotPasswordComponent,
+  },
+  {
+    path: 'adminpatients',
     component: AdminPatientsComponent,
     canActivate: [RoleGuardService],
-    data: { expectedRole: "ROLE_ADMIN" }
+    data: { expectedRole: 'ROLE_ADMIN' },
   },
   {
-    path: "admintasks",
+    path: 'admintasks',
     component: AdminTasksComponent,
     canActivate: [RoleGuardService],
-    data: { expectedRole: "ROLE_ADMIN" }
+    data: { expectedRole: 'ROLE_ADMIN' },
   },
   {
-    path: "history",
+    path: 'history',
     component: HistoryComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
   // TODO: will add RoleGuardService to dashboard
   // TODO: add child components
+
   {
-    path: "admin-dashboard/:selectedView",
+    path: 'admin-dashboard/:selectedView',
     component: AdminDashboardComponent,
-    canActivate: [RoleGuardService],
-    data: { expectedRole: "ROLE_ADMIN" }
+    // canActivate: [RoleGuardService],
+    // data: { expectedRole: "ROLE_ADMIN" }
   },
+  /*
   {
     path: "admin-dashboard",
     component: AdminDashboardComponent,
@@ -101,10 +103,11 @@ const routes: Routes = [
     redirectTo: "admin-dashboard/tasks",
     pathMatch: "full"
   }
+  */
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
