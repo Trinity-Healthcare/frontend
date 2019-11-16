a421bc8f0cbe8f457e7f19ccaf4987fa57bb6354
sethcharris <schristianharris@gmail.com>
2019-11-16 16:31:10 -0600

    Task now updates URL params

--- src/app/admin/admin-redux/admin-redux.component.ts
+++ src/app/admin/admin-redux/admin-redux.component.ts
@@ -1,84 +1,86 @@
-import { Component, OnInit, ViewChild, QueryList, ViewChildren } from '@angular/core';
-import { API, Config, DefaultConfig, APIDefinition } from 'ngx-easy-table';
-import { UserService } from 'src/app/services/user.service';
-import { FullUser } from 'src/app/services/full-user';
-import { UsertaskService } from 'src/app/services/usertask/usertask.service';
-import { RetrievedUserTaskInfo } from 'src/app/services/usertask/retrievedUserTask-info';
-import { TaskServiceService } from 'src/app/services/task/task-service.service';
-import { ActivatedRoute } from '@angular/router';
-import { EventService } from 'src/app/services/event/event.service';
-import { RetrievedTask } from 'src/app/services/task/retrievedTask-info';
-import { CategoryService } from 'src/app/services/category/category.service';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  QueryList,
+  ViewChildren
+} from "@angular/core";
+import { API, Config, DefaultConfig, APIDefinition } from "ngx-easy-table";
+import { UserService } from "src/app/services/user.service";
+import { FullUser } from "src/app/services/full-user";
+import { UsertaskService } from "src/app/services/usertask/usertask.service";
+import { RetrievedUserTaskInfo } from "src/app/services/usertask/retrievedUserTask-info";
+import { TaskServiceService } from "src/app/services/task/task-service.service";
+import { ActivatedRoute } from "@angular/router";
+import { EventService } from "src/app/services/event/event.service";
+import { RetrievedTask } from "src/app/services/task/retrievedTask-info";
+import { CategoryService } from "src/app/services/category/category.service";
+import { Location } from "@angular/common";
 
 @Component({
-  selector: 'app-admin-redux',
-  templateUrl: './admin-redux.component.html',
-  styleUrls: ['./admin-redux.component.css']
+  selector: "app-admin-redux",
+  templateUrl: "./admin-redux.component.html",
+  styleUrls: ["./admin-redux.component.css"]
 })
 export class AdminReduxComponent implements OnInit {
-  @ViewChildren('primaryDataTable') primaryDataTables: QueryList<APIDefinition>;
-  @ViewChild('itemActionsTemplate', { static: false }) actionsTemplate: APIDefinition;
+  @ViewChildren("primaryDataTable") primaryDataTables: QueryList<APIDefinition>;
+  @ViewChild("itemActionsTemplate", { static: false })
+  actionsTemplate: APIDefinition;
 
   public configuration: Config;
-  public selectedView: string = '';
+  public selectedView = "";
   public selectedViewTab: HTMLElement = null;
   public selectedViewData: any = null;
   public serverData: any = null;
 
-  public ADMIN_VIEWS = [
-    'users',
-    'pending',
-    'groups',
-    'tasks',
-    'events'
-  ]
+  public ADMIN_VIEWS = ["users", "pending", "groups", "tasks", "events"];
 
   public readonly ALLOWED_COLUMNS = {
-    'users' : [
-      { key: 'name', title : 'Name'},
-      { key: 'username', title : 'Username' },
-      { key : 'email', title : 'Email' },
-      { key : 'payroll_code', title : 'Payroll Code'},
-      { key : 'week_summary', title : 'Weekly Status'},
-      { key : 'quarter_summary', title : 'Quarterly Status'},
-      { key : 'category.name', title : 'Group' },
-      { key : 'smoking', title : 'Smoking' },
-      { key : 'primary_role', title : 'Role'}
+    users: [
+      { key: "name", title: "Name" },
+      { key: "username", title: "Username" },
+      { key: "email", title: "Email" },
+      { key: "payroll_code", title: "Payroll Code" },
+      { key: "week_summary", title: "Weekly Status" },
+      { key: "quarter_summary", title: "Quarterly Status" },
+      { key: "category.name", title: "Group" },
+      { key: "smoking", title: "Smoking" },
+      { key: "primary_role", title: "Role" }
     ],
 
-    'pending' : [
-      { key: 'userId', title : 'User'},
-      { key: 'associated_task.taskName', title : 'Task'},
-      { key: 'description', title : 'Comments' },
-      { key: 'photo', title : 'Photo' },
-      { key : 'taskPoints', title : 'Points' },
-      { key : 'time', title : 'Time'},
-      { key : 'status', title : 'Status'}
+    pending: [
+      { key: "userId", title: "User" },
+      { key: "associated_task.taskName", title: "Task" },
+      { key: "description", title: "Comments" },
+      { key: "photo", title: "Photo" },
+      { key: "taskPoints", title: "Points" },
+      { key: "time", title: "Time" },
+      { key: "status", title: "Status" }
     ],
 
-    'groups' : [
-      { key: 'name', title : 'Name'},
-      { key: 'description', title : 'Description'},
-      { key: 'quarterly_goal', title : 'Quarterly Goal'}
+    groups: [
+      { key: "name", title: "Name" },
+      { key: "description", title: "Description" },
+      { key: "quarterly_goal", title: "Quarterly Goal" }
     ],
 
-    'tasks' : [
-      { key: 'taskName', title : 'Name'},
-      { key: 'taskAction', title : 'Action'},
-      { key: 'taskPoints', title : 'Points' },
-      { key : 'taskMax', title : 'Maximum' },
-      { key : 'taskFreq', title : 'Time'},
-      { key : 'verificationRequired', title : 'Needs Admin Approval'},
-      { key : 'photoRequired', title : 'Needs Photo'}
+    tasks: [
+      { key: "taskName", title: "Name" },
+      { key: "taskAction", title: "Action" },
+      { key: "taskPoints", title: "Points" },
+      { key: "taskMax", title: "Maximum" },
+      { key: "taskFreq", title: "Time" },
+      { key: "verificationRequired", title: "Needs Admin Approval" },
+      { key: "photoRequired", title: "Needs Photo" }
     ],
 
-    'events' : [
-      { key: 'title', title : 'Name'},
-      { key: 'description', title : 'Description'},
-      { key: 'date', title : 'Points' },
-      { key : 'start', title : 'Start' },
-      { key : 'end', title : 'End'},
-      { key : 'link', title : 'Link'}
+    events: [
+      { key: "title", title: "Name" },
+      { key: "description", title: "Description" },
+      { key: "date", title: "Points" },
+      { key: "start", title: "Start" },
+      { key: "end", title: "End" },
+      { key: "link", title: "Link" }
     ]
   };
 
@@ -88,9 +90,9 @@ export class AdminReduxComponent implements OnInit {
     private taskService: TaskServiceService,
     private eventsService: EventService,
     private categoryService: CategoryService,
-
-    private route: ActivatedRoute,
-  ) { }
+    private location: Location,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.configuration = { ...DefaultConfig };
@@ -100,7 +102,7 @@ export class AdminReduxComponent implements OnInit {
     this.configuration.searchEnabled = false;
 
     this.route.fragment.subscribe((hash: string) => {
-      this.selectedView = hash === null ? this.ADMIN_VIEWS[0] : hash
+      this.selectedView = hash === null ? this.ADMIN_VIEWS[0] : hash;
     });
   }
 
@@ -108,136 +110,145 @@ export class AdminReduxComponent implements OnInit {
     this.processServerData();
   }
 
-  processServerData()
-  {
+  processServerData() {
     this.serverData = {};
 
-    this.userService.getUsers().toPromise().then((data) => {
-      this.serverData['users'] = this.getProcessedUsers(data);
-      return this.taskService.getTasks().toPromise();
-    }).then((data) => {
-      this.serverData['tasks'] = this.getProcessedTasks(data);
-      return this.userTaskService.getAllUserTasks().toPromise();
-    }).then((data) => {
-      this.serverData['pending'] = this.getProcessedPending(data);
-      return this.eventsService.getEvents().toPromise();
-    }).then((data) => {
-      this.serverData['events'] = data;
-      return this.categoryService.getAllCategories().toPromise();
-    }).then((data) => {
-      this.serverData['groups'] = data;
-    }).then(() => {
-      if(this.ADMIN_VIEWS.includes(this.selectedView))
-      { 
-        this.onViewChange('');
-      }
-      else
-      {
-        console.log("An invalid view was specified for the data table.");
-      }
-    }).catch((e) => {
-      console.log(e);
-    });
+    this.userService
+      .getUsers()
+      .toPromise()
+      .then(data => {
+        this.serverData["users"] = this.getProcessedUsers(data);
+        return this.taskService.getTasks().toPromise();
+      })
+      .then(data => {
+        this.serverData["tasks"] = this.getProcessedTasks(data);
+        return this.userTaskService.getAllUserTasks().toPromise();
+      })
+      .then(data => {
+        this.serverData["pending"] = this.getProcessedPending(data);
+        return this.eventsService.getEvents().toPromise();
+      })
+      .then(data => {
+        this.serverData["events"] = data;
+        return this.categoryService.getAllCategories().toPromise();
+      })
+      .then(data => {
+        this.serverData["groups"] = data;
+      })
+      .then(() => {
+        if (this.ADMIN_VIEWS.includes(this.selectedView)) {
+          this.onViewChange("");
+        } else {
+          console.log("An invalid view was specified for the data table.");
+        }
+      })
+      .catch(e => {
+        console.log(e);
+      });
   }
 
-  isServerDataAvailable()
-  {
+  isServerDataAvailable() {
     let isAvailable = true;
 
-    if(!this.serverData)
-    {
+    if (!this.serverData) {
       isAvailable = false;
-    }
-    else
-    {
-      isAvailable = Object.keys(this.serverData).length === this.ADMIN_VIEWS.length;
+    } else {
+      isAvailable =
+        Object.keys(this.serverData).length === this.ADMIN_VIEWS.length;
     }
 
     return isAvailable;
   }
 
-  getProcessedUsers(freshUsers : FullUser[])
-  {
-    freshUsers.forEach((element) => {
-      element['primary_role'] = element.roles[0].name.split('_')[1];
+  getProcessedUsers(freshUsers: FullUser[]) {
+    freshUsers.forEach(element => {
+      element["primary_role"] = element.roles[0].name.split("_")[1];
       //Truthy equals because smoking is a boolean string.
-      element['smoking'] = element['smoker'] == true ? 'Yes' : 'No';
-      element['week_summary'] = `${ element['week_total'] } / ${ element['week_goal'] }`;
-      element['quarter_summary'] = `${ element['quarter_total'] } / ${ element['quarter_goal'] }`;
+      element["smoking"] = element["smoker"] == true ? "Yes" : "No";
+      element[
+        "week_summary"
+      ] = `${element["week_total"]} / ${element["week_goal"]}`;
+      element[
+        "quarter_summary"
+      ] = `${element["quarter_total"]} / ${element["quarter_goal"]}`;
     });
 
     return freshUsers;
   }
 
-  getProcessedPending(freshPending : RetrievedUserTaskInfo[])
-  {
-    freshPending.forEach((element) => {
+  getProcessedPending(freshPending: RetrievedUserTaskInfo[]) {
+    freshPending.forEach(element => {
+      element["associated_task"] = this.serverData.tasks.filter(
+        possibleTask => {
+          return possibleTask.taskId === element.taskId;
+        }
+      );
 
-      element['associated_task'] = this.serverData.tasks.filter((possibleTask) => {
-        return possibleTask.taskId === element.taskId;
-      });
-
-      element['associated_task'] = element['associated_task'][0];
-      element['time'] = new Date(element['completionDate']).toLocaleString('en-US', { timeZone : 'America/Chicago'});
+      element["associated_task"] = element["associated_task"][0];
+      element["time"] = new Date(element["completionDate"]).toLocaleString(
+        "en-US",
+        { timeZone: "America/Chicago" }
+      );
     });
 
     return freshPending;
   }
 
-  getProcessedTasks(freshTasks : RetrievedTask[])
-  {
-    freshTasks.forEach((element) => {
-      element['verificationRequired'] = Boolean(element['verificationRequired']) === true ? 'Yes' : 'No';
-      element['photoRequired'] = Boolean(element['photoRequired']) === true ? 'Yes' : 'No';
+  getProcessedTasks(freshTasks: RetrievedTask[]) {
+    freshTasks.forEach(element => {
+      element["verificationRequired"] =
+        Boolean(element["verificationRequired"]) === true ? "Yes" : "No";
+      element["photoRequired"] =
+        Boolean(element["photoRequired"]) === true ? "Yes" : "No";
     });
 
     return freshTasks;
   }
-  
-  getDataColumns(base_columns)
-  {
-    let actionTemplate = { key : 'isActive', title : 'Actions',  cellTemplate: this.actionsTemplate };
+
+  getDataColumns(base_columns) {
+    let actionTemplate = {
+      key: "isActive",
+      title: "Actions",
+      cellTemplate: this.actionsTemplate
+    };
     let viewColumns = base_columns;
 
-    if(viewColumns[viewColumns.length - 1].key !== 'isActive')
-    {
+    if (viewColumns[viewColumns.length - 1].key !== "isActive") {
       viewColumns.push(actionTemplate);
     }
 
     return viewColumns;
   }
 
-  editItem(item : any) {
+  editItem(item: any) {
     console.log(item);
   }
 
-  deleteItem(item : any) {
+  deleteItem(item: any) {
     console.log(item);
   }
 
-  onViewChange(newView : string)
-  {
-    if(this.selectedView && newView !== this.selectedView && newView !== '')
-    {
+  onViewChange(newView: string) {
+    if (this.selectedView && newView !== this.selectedView && newView !== "") {
       this.selectedView = newView;
     }
+    this.location.replaceState(`/admin-redux#${this.selectedView}`);
   }
 
   onSearchChange(name: string): void {
-    this.primaryDataTables.forEach((child) => {
+    this.primaryDataTables.forEach(child => {
       child.apiEvent({
-        type: API.onGlobalSearch, value: name,
+        type: API.onGlobalSearch,
+        value: name
       });
     });
   }
 
-  toUppercase(s : string)
-  {
+  toUppercase(s: string) {
     return s.charAt(0).toUpperCase() + s.slice(1);
   }
 
-  toLowercase(s : string)
-  {
+  toLowercase(s: string) {
     return s.charAt(0).toLowerCase() + s.slice(1);
   }
 }
